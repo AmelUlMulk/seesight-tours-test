78e139c1078adedc0eaedb17695f54aedd7b98d7
HammadHassan7991 <112782383+HammadHassan7991@users.noreply.github.com>
2022-12-28 19:48:36 +0500

    update gradient banner

--- components/Contact/PageHero.tsx
+++ components/Contact/PageHero.tsx
@@ -1,18 +1,21 @@
 import styled from 'styled-components';
 
 const PageHero = () => {
-  const PageHero_Section = styled.div`
-    backgroudimage: linear-gradient(rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.35)),
+  const PageHero = styled.div`
+    height: 50vh;
+    background-image: linear-gradient(
+        to top,
+        rgba(0, 0, 0, 0.35),
+        rgba(0, 0, 0, 0.35)
+      ),
       url('https://res.cloudinary.com/see-sight-tours/image/upload/q_auto,f_auto,c_fill,g_faces,h_570,w_958,y_0/v1582036498/Happy-group-tour-guides.jpg');
+    z-index: -2;
   `;
 
   return (
-    <section className="Hero_Section opacity-70">
-      <div
-        className="PageHero h-[50vh] bg-no-repeat bg-cover bg-center bg-gradient-to-r from-purple-600 to-blue-600 flex justify-start items-center"
-        style={{
-          backgroundImage: `url("https://res.cloudinary.com/see-sight-tours/image/upload/q_auto,f_auto,c_fill,g_faces,h_570,w_958,y_0/v1582036498/Happy-group-tour-guides.jpg")`
-        }}
+    <section className="Hero_Section ">
+      <PageHero
+        className="PageHero bg-no-repeat bg-cover bg-center flex justify-start items-center"
       >
         <div className="PageHero_container flex flex-col pl-10 ">
           <div className="text-5xl font-bold text-white">
@@ -22,7 +25,7 @@ const PageHero = () => {
             <p>We&apos;d love to hear from you</p>
           </div>
         </div>
-      </div>
+      </PageHero>
     </section>
   );
 };
