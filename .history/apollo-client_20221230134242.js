b5fae76f7430ee5e8c8ddd5226b29b9ea6184cfe
maha <maharizwan321@gmail.com>
2022-12-31 21:55:50 -0800

    Graphql integration

--- .history/apollo-client_20221230134242.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-    uri: "https://boatapp.seesight-tours.com/console/",
-    cache: new InMemoryCache(),
-});
-
-export default client;
