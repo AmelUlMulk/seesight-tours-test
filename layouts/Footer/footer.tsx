5e9ec9d89d216d38b5ef0f19f0cc4d19db57b824
HammadHassan7991 <112782383+HammadHassan7991@users.noreply.github.com>
2023-01-05 13:23:29 +0500

    apply camel case

--- layouts/Footer/footer.tsx
+++ layouts/Footer/footer.tsx
@@ -10,15 +10,15 @@ import ClockIcon from '../../assets/svg/Time.svg';
 import MobileIcon from '../../assets/svg/phone-icon.svg';
 import MessageIcon from '../../assets/svg/Message.svg';
 interface Props {
-  IsActive: boolean;
+  isActive: boolean;
 }
-const Link_Style = styled.div<Props>`
-  color: ${props => (props.IsActive ? 'red' : 'inherit')};
+const LinkStyle = styled.div<Props>`
+  color: ${props => (props.isActive ? 'red' : 'inherit')};
 `;
 const Footer = () => {
   const router = useRouter();
   return (
-    <section id="Footer_Section ">
+    <section id="footer_section ">
       <div className="flex px-20">
         <div className="flex flex-wrap  gap-14 max-w-[1200px] mx-auto w-[100%] ">
           <div className="logo-section w-[100%] lg:w-[35%] sm:w-[50%] xsm:w-[90%] md:w-[40%]">
@@ -39,101 +39,101 @@ const Footer = () => {
               electronic
             </div>
           </div>
-          <div id="Book_your_tour mt-5">
+          <div id="book_your_tour mt-5">
             <h1 className="text-2xl font-bold">Book your Tour</h1>
             <ul>
               <li className="py-2">
                 <Link href="/tours">
-                  <Link_Style IsActive={router.asPath === '/tours'}>
+                  <LinkStyle isActive={router.asPath === '/tours'}>
                     Day Tours
-                  </Link_Style>
+                  </LinkStyle>
                 </Link>
               </li>
               <li className="py-2">
                 <Link href="/multi-day-tours">
-                  <Link_Style IsActive={router.asPath === '/multi-day-tours'}>
+                  <LinkStyle isActive={router.asPath === '/multi-day-tours'}>
                     Mutli Day Tours
-                  </Link_Style>
+                  </LinkStyle>
                 </Link>
               </li>
               <li className="py-2">
                 <Link href="/cities">
-                  <Link_Style IsActive={router.asPath === '/cities'}>
+                  <LinkStyle isActive={router.asPath === '/cities'}>
                     Discover Cities
-                  </Link_Style>
+                  </LinkStyle>
                 </Link>
               </li>
               <li className="py-2">
                 <Link href="trip-shepherd">
-                  <Link_Style IsActive={router.asPath === '/trip-shepherd'}>
+                  <LinkStyle isActive={router.asPath === '/trip-shepherd'}>
                     Trip Shepherd
-                  </Link_Style>
+                  </LinkStyle>
                 </Link>
               </li>
               <li className="py-2">
                 <Link href="/faq">
-                  <Link_Style IsActive={router.asPath === '/faq'}>
+                  <LinkStyle isActive={router.asPath === '/faq'}>
                     FAQ&apos;s
-                  </Link_Style>
+                  </LinkStyle>
                 </Link>
               </li>
               <li className="py-2">
                 <Link href="/reviews">
-                  <Link_Style IsActive={router.asPath === '/reviews'}>
+                  <LinkStyle isActive={router.asPath === '/reviews'}>
                     Reviews
-                  </Link_Style>
+                  </LinkStyle>
                 </Link>
               </li>
             </ul>
           </div>
-          <div id="Menu">
+          <div id="menu">
             <h1 className="text-2xl font-bold">Menu</h1>
             <ul className="mt-5">
               <li className="py-2">
                 <Link href="/blog">
-                  <Link_Style IsActive={router.asPath === '/blog'}>
+                  <LinkStyle isActive={router.asPath === '/blog'}>
                     Day Tours
-                  </Link_Style>
+                  </LinkStyle>
                 </Link>
               </li>
               <li className="py-2">
                 <Link href="/privacy">
-                  <Link_Style IsActive={router.asPath === '/privacy'}>
+                  <LinkStyle isActive={router.asPath === '/privacy'}>
                     Privacy Policy
-                  </Link_Style>
+                  </LinkStyle>
                 </Link>
               </li>
               <li className="py-2">
                 <Link href="/about">
-                  <Link_Style IsActive={router.asPath === '/about'}>
+                  <LinkStyle isActive={router.asPath === '/about'}>
                     About Us
-                  </Link_Style>
+                  </LinkStyle>
                 </Link>
               </li>
               <li className="py-2">
                 <Link href="/contact">
-                  <Link_Style IsActive={router.asPath === '/contact'}>
+                  <LinkStyle isActive={router.asPath === '/contact'}>
                     Contact Us
-                  </Link_Style>
+                  </LinkStyle>
                 </Link>
               </li>
               <li className="py-2">
                 <Link href="/careers">
-                  <Link_Style IsActive={router.asPath === '/careers'}>
+                  <LinkStyle isActive={router.asPath === '/careers'}>
                     Careers
-                  </Link_Style>
+                  </LinkStyle>
                 </Link>
               </li>
               <li className="py-2">
                 <Link href="/partner">
-                  <Link_Style IsActive={router.asPath === '/partner'}>
+                  <LinkStyle isActive={router.asPath === '/partner'}>
                     Become a Partner
-                  </Link_Style>
+                  </LinkStyle>
                 </Link>
               </li>
             </ul>
           </div>
-          <div id="Get_in_touch">
+          <div id="get_in_touch">
             <h1 className="text-2xl font-bold">Get in Touch</h1>
             <ul className="mt-5">
               <li className="py-2 flex gap-5 items-center">
@@ -171,7 +171,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div id="Follow_us" className="mt-2 ">
+      <div id="follow_us" className="mt-2 ">
         <div className="w-[80%] lg:w-[40%] md:w-[60%] sm:w-[70%] xsm:w-[95%] ml-auto flex flex-wrap gap-11 xsm:gap-8 xxsm:gap-5 justify-center items-center bg-blue-500 text-2xl xsm:text-[1.20rem] text-center text-white rounded-l-[25px] py-3">
           <div>Follow us</div>
           <a
