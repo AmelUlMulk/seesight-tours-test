9142a1cc610a60c02819803c33bc120884e307b7
maha <maharizwan321@gmail.com>
2023-01-11 15:20:20 -0800

    Missing props fixed

--- pages/guides.tsx
+++ pages/guides.tsx
@@ -9,6 +9,7 @@ const Guides = () => {
       media={
         'https://res.cloudinary.com/see-sight-tours/image/upload/w_1920,h_500/t_header/f_auto,q_auto,/fl_progressive:steep/v1581435096/american-falls-luna-island-rainbow-bridge.webP'
       }
+      video={false}
     />
   );
 };
