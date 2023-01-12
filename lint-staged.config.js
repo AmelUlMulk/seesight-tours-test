1d94e08dd7c64e4484d98cb872359f024a4c7a05
HammadHassan7991 <112782383+HammadHassan7991@users.noreply.github.com>
2023-01-12 16:16:59 +0500

    ls-test-1

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
+  '**/*.(ts|tsx)': () => 'npx tsc --pretty --noEmit',
 
   // Lint then format TypeScript and JavaScript files
   '**/*.(ts|tsx|js)': filenames => [
-    `npx eslint --fix ${filenames.join(' ')}`,
+    `npx eslint --ext ts --ext tsx --ext js ${filenames.join(' ')}`,
     `npx prettier --write ${filenames.join(' ')}`
   ]
 };
