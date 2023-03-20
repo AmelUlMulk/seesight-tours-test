a4479de90b6447bfbc575e7cd51ad71c840f99bf
Amel Ul Mulk <shahzadaamel@gmail.com>
2023-03-20 13:33:12 +0500

    pushing iwht hope of building

--- pages/tours/[slug].tsx
+++ pages/tours/[slug].tsx
@@ -4,12 +4,10 @@ import FETCH_PRODUCT, {
   FETCH_PRODUCT_INTERFACE,
   PRODUCTINTERFACE1
 } from '../../api/tourPage';
-import Image from 'next/image';
+
 import client from '../../apollo-client';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import SwiperCore, { Navigation } from 'swiper';
-import styled from 'styled-components';
 import HeroSwipper from '../../components/TourPage/HeroSwipper';
 import TourBasics from '../../components/TourPage/TourBasics';
 import Included from '../../components/TourPage/Included';
@@ -18,15 +16,16 @@ import Newsletter from '../../layouts/Newsletter/Newsletter';
 const TourPage = ({ product, reviews }: PRODUCTPAGEINTERFACE) => {
   /* const swiper = useSwiper(); */
 
-  console.log('this is the thing', product);
-
-  SwiperCore.use([Navigation]);
-
   return (
     <div className="flex flex-col ">
       <h1 className="w-full text-2xl lg:text-3xl xl:text-5xl  font-extrabold text-center pt-12  pb-7  ">
-        {product.cities[0].name} <span className=" font-black ">{`>`}</span>{' '}
-        {product.name}{' '}
+        {product.cities[0] && (
+          <>
+            {product.cities[0].name}
+            <span className=" font-black "> {`>`}</span> {product.name}{' '}
+          </>
+        )}{' '}
+        {}{' '}
       </h1>
       <HeroSwipper media={product.carousel} />
       <TourBasics
@@ -36,7 +35,11 @@ const TourPage = ({ product, reviews }: PRODUCTPAGEINTERFACE) => {
         rating={reviews.average}
         totalRatings={reviews.total}
       />
-      <Included data={product.tourIncludes} attractions={product.attractions} />
+      <Included
+        longDescription={product.longDescription}
+        data={product.tourIncludes}
+        attractions={product.attractions}
+      />
       <Newsletter />
     </div>
   );
@@ -64,7 +67,7 @@ export async function getStaticPaths() {
   const { data } = await client.query<PRODUCTSLUGS>({
     query: gql`
       query PRODUCTS {
-        products: boatnew_products(where: { slug: { _neq: "null" } }) {
+        products {
           slug
           name
         }
@@ -76,26 +79,32 @@ export async function getStaticPaths() {
     paths: data.products.map(product => {
       return { params: { slug: product.slug.toString() } };
     }),
-    fallback: true
+    fallback: false
   };
 }
 export async function getStaticProps({ params }: PARAMS) {
   const { slug } = params;
-
   const { data } = await client.query<FETCH_PRODUCT_INTERFACE>({
     query: FETCH_PRODUCT,
     variables: {
       slug
     }
   });
-  console.log('these are the reviews', data.reviews);
+  if (!data.product) {
+    return {
+      redirect: {
+        statusCode: 301,
+        destination: '/'
+      }
+    };
+  }
   const reviews = {
     average: Number(
       data.reviews.reduce((a, b) => a + b.rating, 0) / data.reviews.length
     ).toFixed(1),
     total: data.reviews.length
   };
-  console.log('these are the reviews', reviews);
+
   return {
     props: {
       product: data.product[0],
