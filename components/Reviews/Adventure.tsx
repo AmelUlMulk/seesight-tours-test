ab53431a4be31a5460d7a0d08ed592bbb1223277
HammadHassan7991 <112782383+HammadHassan7991@users.noreply.github.com>
2023-03-21 18:57:22 +0500

    page hero searchcity and newletter section added

--- components/Reviews/Adventure.tsx
+++ components/Reviews/Adventure.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image';
 import styled from 'styled-components';
 import PageHero from '../../layouts/PageHero';
+import SearchCity from '../Searchbar/searchCity';
 
 const AdventureStyle = styled.div`
   background-image: url('/review_adventure.png');
 `;
 const Adventure = () => {
   return (
-    <AdventureStyle className="bg-no-repeat bg-cover bg-center h-[40vh] flex justify-center items-center mt-5">
+    <AdventureStyle className="bg-no-repeat bg-cover bg-center h-[45vh] flex flex-col justify-center items-center mt-5">
       <div className="text-white text-center max-w-[88%]">
         <h1 className=" text-[20px] sm:text-[25px] md:text-[30px] lg:text-[40px] xl:text-[56px] 2xl:text-[66px] font-[400] ">
           Ready for an adventure?
@@ -16,6 +17,9 @@ const Adventure = () => {
           The best small group tours operating across Canada and the United
           States
         </p>
+      </div>
+      <div className="relative my-10 w-[100%]">
+        <SearchCity />
       </div>
     </AdventureStyle>
   );
