48274319dd6aaefccdd3a27a658b882c7a9276ac
HammadHassan7991 <112782383+HammadHassan7991@users.noreply.github.com>
2023-04-17 15:26:18 +0500

    recent booking notification added

---
+++ api/socialProofs.ts
@@ -0,0 +1,66 @@
+import { gql } from '@apollo/client';
+
+export const RECENT_BOOKINGS = gql`
+  query RECENT_BOOKINGS($pastDate: timestamptz, $tourSlug: String) {
+    bookings: boatnew_booking_aggregate(
+      where: {
+        created_at: { _gte: $pastDate }
+        product: { slug: { _eq: $tourSlug } }
+      }
+    ) {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
+export const CONFIRMED_BOOKING_BY_DATE = gql`
+  query CONFIRMED_BOOKING_BY_DATE($pastDate: timestamptz, $today: timestamptz) {
+    bookings: boatnew_booking(
+      where: {
+        status: { _eq: "CONFIRMED" }
+        created_at: { _gte: $pastDate, _lte: $today }
+      }
+      order_by: { created_at: desc }
+    ) {
+      customer {
+        customerName: contact_name
+      }
+      product {
+        citiesProducts: cities_products {
+          city {
+            name
+          }
+        }
+      }
+      createdAt: created_at
+    }
+  }
+`;
+export interface RECENTBOOKINGS {
+  bookings: {
+    aggregate: {
+      count: number;
+    };
+  };
+}
+export interface RECENTCONFIRMEDBOOKINGS {
+  bookings: [
+    {
+      customer: {
+        customerName: string;
+      };
+      product: {
+        citiesProducts: [
+          {
+            city: {
+              name: string;
+            };
+          }
+        ];
+      };
+
+      createdAt: Date;
+    }
+  ];
+}
---
+++ components/Notifications/recentBookings.tsx
@@ -0,0 +1,60 @@
+import { useLazyQuery } from '@apollo/client';
+import dayjs from 'dayjs';
+import {
+  CONFIRMED_BOOKING_BY_DATE,
+  RECENTCONFIRMEDBOOKINGS
+} from '../../api/socialProofs';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
+
+const RecentConfirmBookings = () => {
+  const [triggerBookings, { data, loading, error }] =
+    useLazyQuery<RECENTCONFIRMEDBOOKINGS>(CONFIRMED_BOOKING_BY_DATE);
+
+  const timeIdArray: number[] = [];
+
+  useEffect(() => {
+    const bookingDelay = setTimeout(() => {
+      triggerBookings({
+        variables: {
+          pastDate: dayjs().subtract(1, 'days').format('YYYY-MM-DD'),
+          today: dayjs().format('YYYY-MM-DD')
+        }
+      });
+      //clean up
+      return () => clearTimeout(bookingDelay);
+    }, 10000);
+  }, []);
+
+  useEffect(() => {
+    data?.bookings.map((booking, index) => {
+      const recall = window.setTimeout(() => {
+        toast.info(
+          <div>
+            <div>
+              <span id="emoji-face">🤩</span>
+              <span>
+                {' '}
+                {booking.product.citiesProducts[0].city.name.split(' ')[0]}
+              </span>
+            </div>
+
+            <p>
+              Confirmed a tour of {booking.product.citiesProducts[0].city.name}{' '}
+              {dayjs().diff(dayjs(booking.createdAt), 'hour')} hours ago
+            </p>
+            {/* <p>{booking.customer.customerName}</p> */}
+          </div>
+        );
+      }, index * 20000);
+
+      timeIdArray.push(recall);
+    });
+    return () => {
+      timeIdArray.map(timerId => clearTimeout(timerId));
+    };
+  }, [data]);
+  return null;
+};
+
+export default RecentConfirmBookings;
