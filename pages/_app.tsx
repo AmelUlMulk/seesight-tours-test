93c725d92b67a25201378dd0aae1aa6ec42915af
HammadHassan7991 <112782383+HammadHassan7991@users.noreply.github.com>
2023-04-28 18:28:52 +0500

    remove extra things

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,29 +13,6 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    //This is done so that the page analytics are not called twice, once intially and once when the component loads on reload.
-    window.history.replaceState({}, document.title);
-    const analyticDelayedCall = setTimeout(() => {
-      if (
-        (router.pathname === '/' && router?.query?.prevPath?.length) ??
-        0 < 0
-      ) {
-        return;
-      }
-      // @ts-ignore
-      if (window.gtag) {
-        // @ts-ignore
-        window.gtag('config', 'UA-8671114-2', {
-          page_title: router.pathname === undefined ? '/' : router.pathname,
-          page_path: router.pathname === undefined ? '/' : router.pathname
-        });
-      }
-    }, 0);
-    return () => clearTimeout(analyticDelayedCall);
-  }, [router.pathname, router.query]);
-
   return (
     <>
       {process.env.NODE_ENV === 'production' && (
@@ -44,20 +21,16 @@ export default function App({ Component, pageProps }: AppProps) {
             strategy="lazyOnload"
             src="https://www.googletagmanager.com/gtag/js?id=UA-8671114-2"
           />
-          <Script
-            id="google-analytics"
-            strategy="lazyOnload"
-            dangerouslySetInnerHTML={{
-              __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'UA-8671114-2', {
-                    page_path: window.location.pathname,
-                    });
-                `
-            }}
-          />
+          <Script id="google-analytics" strategy="lazyOnload">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-8671114-2', {
+          page_path: window.location.pathname,
+          });
+      `}
+          </Script>
         </>
       )}
 
