10392effce43cfb81e5eeae8ac0c472b16942fa2
Amel Ul Mulk <shahzadaamel@gmail.com>
2023-05-12 12:03:47 +0500

    delay analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,21 +11,29 @@ import dynamic from 'next/dynamic';
 
 const PaxProvider = dynamic(() => import('../utils/checkoutContext'));
 import NavBar from '../layouts/NavBar';
+import { useEffect, useState } from 'react';
 const AppoloProvider = dynamic(() => import('../utils/dynamicAppolo'), {
   ssr: false
 });
 export default function App({ Component, pageProps }: AppProps) {
+  const [delayAnalyticsLoad, setAnalyticsLoad] = useState<boolean>(false);
+
+  useEffect(() => {
+    const analyticsTimeout = setTimeout(() => {
+      setAnalyticsLoad(true);
+    }, 7000);
+    return () => clearTimeout(analyticsTimeout);
+  }, []);
+
   return (
     <>
-      {process.env.NODE_ENV === 'production' && (
+      {process.env.NODE_ENV === 'production' && delayAnalyticsLoad && (
         <>
           <Script
             strategy="lazyOnload"
             src="https://www.googletagmanager.com/gtag/js?id=UA-8671114-2"
-            defer
-            async
           />
-          <Script id="google-analytics" strategy="lazyOnload" defer async>
+          <Script id="google-analytics" strategy="lazyOnload">
             {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
